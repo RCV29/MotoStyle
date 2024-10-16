ff244c12083992819897cef35a2590646157f8c3
RCV29 <roco.visperas.up@phinmaed.com>
2024-10-16 20:34:09 +0800

    50 pictures

--- resources/views/community/show.blade.php
+++ resources/views/community/show.blade.php
@@ -8,7 +8,6 @@
                         <img src="{{ asset($item->image) }}" class="w-32 h-32 object-cover rounded" alt="img"/>
                     </div>
                     <h2 class="text-lg font-semibold mb-2 text-center">{{ $item->name }}</h2>
-                    <p class="text-gray-700 text-center">{{ $item->description }}</p>
 
                     <div class="mt-4 flex justify-center">
                         <a href="{{ route('community.edit', $item->id) }}" class="bg-gray-800 text-white border-2 border-gray-800 px-4 py-2 rounded-md hover:bg-gray-700 transition duration-200" style="margin-right: 10px;">
